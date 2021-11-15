473cc600c0081530f9e9d6172186d3f15da9a69a
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2021-11-15 11:52:23 +0100

    added iostream

--- coursStephane/main.cpp
+++ coursStephane/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main() {
+
+    cout << "Bienvenue juste prix" << endl;
+
+    auto aDeviner{0};
+
+    cin >> aDeviner;
+    // afficher la variable
+    cout << aDeviner;
 }
+
